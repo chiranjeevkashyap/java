e5872864b5bc623a706fefddc773ce41f2882af6
Chiranjeev Kashyap <developer.chiranjeev@gmail.com>
2025-07-18 11:19:58 +0530

    Update Query.java
    moved to advance & independent path

--- java/src/main/java/com/chiranjeevkashyap/query/Query.java
+++ java/src/main/java/com/chiranjeevkashyap/query/Query.java
@@ -1,26 +1,28 @@
 package com.chiranjeevkashyap.query;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public enum Query {
-    SELECT_EMP("select.sql");
-    private final Path path;
+    SELECT_EMP("sql/select.sql");
 
-    Query(String path) {
-        if (path == null) {
+    private final String name;
+
+    Query(String name) {
+        if (name == null) {
             throw new NullPointerException("Path must not be null");
         }
-        if (path.trim().isEmpty()) {
+        if (name.trim().isEmpty()) {
             throw new IllegalArgumentException("Path must not be empty");
         }
-        this.path = Paths.get("java/src/main/resources/sql/" + path);
+        this.name = name;
     }
 
     public String getQuery() throws IOException {
-        return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(name)) {
+            if (inputStream == null) throw new NullPointerException("SQL file not found in resources: " + name);
+            return new Scanner(inputStream, "UTF-8").useDelimiter("\\A").next().trim();
+        }
     }
 }
